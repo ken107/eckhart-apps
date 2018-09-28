2abf6249d362557ef755abfa7e7035808939c09d
Hai Phan <hai.phan@gmail.com>
2018-09-28 08:51:01 -0700

    jest tests

--- service/test.js
+++ service/test.js
@@ -1,11 +1,16 @@
-const app = require("./scrape");
+const scrape = require("./scrape");
 
-const method = process.argv[2];
-const args = Array.prototype.slice.call(process.argv, 3);
-console.log(method, args);
+test("eckhart listIssues", async () => {
+  const result = await scrape.listIssues();
+  expect(result).toHaveProperty("0.id", expect.any(String));
+})
 
-if (!app[method]) throw new Error("Bad method");
+test("eckhart listVideos", async () => {
+  const result = await scrape.listVideos("DEC2015");
+  expect(result).toHaveProperty("0.title", "Transcending Our Stories");
+})
 
-app[method].apply(null, args)
-  .then(console.log)
-  .catch(console.error)
+test("eckhart getVideo", async () => {
+  const result = await scrape.getVideo("6fe69a59-4f02-44ad-acc0-9bb28b75c744");
+  expect(result).toBe("https://s3.amazonaws.com/etnow/dec2015_060415qa01_hypocrite_128k.mp3");
+})
